b0430e10fb58bdd3c32b8ed2a4671eb51840ce72
CodeSandbox <support@codesandbox.io>
2022-11-23 15:07:08 +0000

    Initial commit

---
+++ Day_1/main.py
@@ -0,0 +1,4 @@
+print("Hello User!\nWelcome to Band Name Generator")
+city = input("Which city you grew up in?\n")
+pet = input("What's the name of your pet?\n")
+print("Your Band Name could be " + city + " " + pet)
